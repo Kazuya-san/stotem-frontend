9380568c454445684ee2f3ff05d314890651a5c5
Kazuya-san <kazuyasan3286@gmail.com>
2022-12-16 16:02:25 +0500

    fixed bugs8

--- src/pages/SingleEvent.jsx
+++ src/pages/SingleEvent.jsx
@@ -149,7 +149,7 @@ const SingleEvent = () => {
           }}
         >
           {imageLoading && (
-            <div className="flex items-center justify-center h-[350px]">
+            <div className="flex items-center justify-center h-[350px] w-screen">
               <div className="flex justify-center items-center h-full">
                 <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
               </div>
